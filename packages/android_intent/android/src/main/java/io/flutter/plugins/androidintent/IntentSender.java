91296be737afb6fe1915ba7477709a0284673c6c
Hari Nugroho <harinugroho@users.noreply.github.com>
2021-05-01 05:17:45 +0700

    Update IntentSender.java

--- packages/android_intent/android/src/main/java/io/flutter/plugins/androidintent/IntentSender.java
+++ packages/android_intent/android/src/main/java/io/flutter/plugins/androidintent/IntentSender.java
@@ -5,11 +5,24 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Forms and launches intents. */
 public final class IntentSender {
@@ -59,7 +72,7 @@ public final class IntentSender {
     }
   }
 
-  void shareFiles(List<String> paths, List<String> mimeTypes, String text, String subject)
+  void shareFiles(Intent intent, List<String> paths, List<String> mimeTypes)
           throws IOException {
     if (paths == null || paths.isEmpty()) {
       throw new IllegalArgumentException("Non-empty path expected");
@@ -68,33 +81,30 @@ public final class IntentSender {
     clearExternalShareFolder();
     ArrayList<Uri> fileUris = getUrisForPaths(paths);
 
-    Intent shareIntent = new Intent();
     if (fileUris.isEmpty()) {
-      share(text, subject);
+      send(intent);
       return;
     } else if (fileUris.size() == 1) {
-      shareIntent.setAction(Intent.ACTION_SEND);
-      shareIntent.putExtra(Intent.EXTRA_STREAM, fileUris.get(0));
-      shareIntent.setType(
+      intent.setAction(Intent.ACTION_SEND);
+      intent.putExtra(Intent.EXTRA_STREAM, fileUris.get(0));
+      intent.setType(
               !mimeTypes.isEmpty() && mimeTypes.get(0) != null ? mimeTypes.get(0) : "*/*");
     } else {
-      shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
-      shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, fileUris);
-      shareIntent.setType(reduceMimeTypes(mimeTypes));
+      intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+      intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, fileUris);
+      intent.setType(reduceMimeTypes(mimeTypes));
     }
-    if (text != null) shareIntent.putExtra(Intent.EXTRA_TEXT, text);
-    if (subject != null) shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-    Intent chooserIntent = Intent.createChooser(shareIntent, null /* dialog title optional */);
+    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    Intent chooserIntent = Intent.createChooser(intent, null /* dialog title optional */);
 
     List<ResolveInfo> resInfoList =
-            getContext()
+            applicationContext
                     .getPackageManager()
                     .queryIntentActivities(chooserIntent, PackageManager.MATCH_DEFAULT_ONLY);
     for (ResolveInfo resolveInfo : resInfoList) {
       String packageName = resolveInfo.activityInfo.packageName;
       for (Uri fileUri : fileUris) {
-        getContext()
+        applicationContext
                 .grantUriPermission(
                         packageName,
                         fileUri,
@@ -102,9 +112,110 @@ public final class IntentSender {
       }
     }
 
-    startActivity(chooserIntent);
+    applicationContext.startActivity(chooserIntent);
   }
 
+  @SuppressWarnings("ResultOfMethodCallIgnored")
+  private void clearExternalShareFolder() {
+    File folder = getExternalShareFolder();
+    if (folder.exists()) {
+      for (File file : folder.listFiles()) {
+        file.delete();
+      }
+      folder.delete();
+    }
+  }
+
+  private String reduceMimeTypes(List<String> mimeTypes) {
+    if (mimeTypes.size() > 1) {
+      String reducedMimeType = mimeTypes.get(0);
+      for (int i = 1; i < mimeTypes.size(); i++) {
+        String mimeType = mimeTypes.get(i);
+        if (!reducedMimeType.equals(mimeType)) {
+          if (getMimeTypeBase(mimeType).equals(getMimeTypeBase(reducedMimeType))) {
+            reducedMimeType = getMimeTypeBase(mimeType) + "/*";
+          } else {
+            reducedMimeType = "*/*";
+            break;
+          }
+        }
+      }
+      return reducedMimeType;
+    } else if (mimeTypes.size() == 1) {
+      return mimeTypes.get(0);
+    } else {
+      return "*/*";
+    }
+  }
+
+  @NonNull
+  private String getMimeTypeBase(String mimeType) {
+    if (mimeType == null || !mimeType.contains("/")) {
+      return "*";
+    }
+
+    return mimeType.substring(0, mimeType.indexOf("/"));
+  }
+
+  private ArrayList<Uri> getUrisForPaths(List<String> paths) throws IOException {
+    ArrayList<Uri> uris = new ArrayList<>(paths.size());
+    for (String path : paths) {
+      File file = new File(path);
+      if (!fileIsOnExternal(file)) {
+        file = copyToExternalShareFolder(file);
+      }
+      uris.add(
+              FileProvider.getUriForFile(
+                      applicationContext, applicationContext.getPackageName() + ".flutter.share_provider", file));
+    }
+    return uris;
+  }
+
+  @SuppressWarnings("ResultOfMethodCallIgnored")
+  private File copyToExternalShareFolder(File file) throws IOException {
+    File folder = getExternalShareFolder();
+    if (!folder.exists()) {
+      folder.mkdirs();
+    }
+
+    File newFile = new File(folder, file.getName());
+    copy(file, newFile);
+    return newFile;
+  }
+
+  @NonNull
+  private File getExternalShareFolder() {
+    return new File(applicationContext.getExternalCacheDir(), "share");
+  }
+
+  private boolean fileIsOnExternal(File file) {
+    try {
+      String filePath = file.getCanonicalPath();
+      File externalDir = applicationContext.getExternalFilesDir(null);
+      return externalDir != null && filePath.startsWith(externalDir.getCanonicalPath());
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  private static void copy(File src, File dst) throws IOException {
+    InputStream in = new FileInputStream(src);
+    try {
+      OutputStream out = new FileOutputStream(dst);
+      try {
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+          out.write(buf, 0, len);
+        }
+      } finally {
+        out.close();
+      }
+    } finally {
+      in.close();
+    }
+  }
   /**
    * Verifies the given intent and returns whether the application context class can resolve it.
    *
